0a64366872629fb91c3973ddbb0f4a0d30b5f8ec
Lige <lige.williamson@gmail.com>
2019-03-01 20:23:41 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,6 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   sorted = arr.sort_by do |phrase|
-    binding.pry
     indicies = []
     phrase.each_char{|index| indicies << esperanto_alphabet.index(phrase[index])}
     indicies
